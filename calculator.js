edd0d5839939e07edaada3c88ef7a78a78067fa4
Pareeshay Syed <137789313+123paree@users.noreply.github.com>
2024-03-29 21:28:37 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { type: "number", name: "num1", message: "Enter num1" },
+    { type: "number", name: "num2", message: "Enter num2" },
+    {
+        type: "list",
+        name: "operators",
+        message: "Select one of the operator",
+        choices: ["+", "-", "*", "/", "%"],
+    },
+]);
+if (answer.operators === "+") {
+    console.log((answer.num1 + answer.num2).toFixed(2));
+}
+else if (answer.operators === "-") {
+    console.log((answer.num1 - answer.num2).toFixed(2));
+}
+else if (answer.operators === "*") {
+    console.log((answer.num1 * answer.num2).toFixed(2));
+}
+else if (answer.operators === "/") {
+    console.log((answer.num1 / answer.num2).toFixed(2));
+}
+else if (answer.operators === "%") {
+    console.log(answer.num1 % answer.num2);
+}
+else {
+    console.log("Please select valid operator");
+}
